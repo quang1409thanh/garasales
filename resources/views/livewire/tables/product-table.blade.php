578e30c1ebebde0611b2b27ee63c6b02beb27625
thanhyk14 <thanhaxt342@gmail.com>
2025-04-17 22:56:44 +0700

    edit view

--- resources/views/livewire/tables/product-table.blade.php
+++ resources/views/livewire/tables/product-table.blade.php
@@ -39,8 +39,10 @@
                     <select wire:model.live="perPage" class="form-select form-select-sm" aria-label="result per page">
                         <option value="5">5</option>
                         <option value="10">10</option>
-                        <option value="15">15</option>
-                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                        <option value="200">200</option>
+                        <option value="500">500</option>
                     </select>
                 </div>
             </div>
